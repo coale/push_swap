4fb5b78f9b347716ffcb879160c146aa346b40fd
coale <alessandra.conta@gmail.com>
2023-05-16 18:37:00 +0200

    norminette

--- checks1.c
+++ checks1.c
@@ -6,7 +6,7 @@
 /*   By: alessandra <alessandra@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 14:05:50 by aconta            #+#    #+#             */
-/*   Updated: 2023/05/16 18:34:18 by alessandra       ###   ########.fr       */
+/*   Updated: 2023/05/16 18:36:39 by alessandra       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,10 +42,7 @@ int	is_digit_and_sign(char *str)
 		if ((str[i] >= '0' && str[i] <= '9'))
 			i++;
 		else
-		{
-			//ft_putendl_fd("Error", 2);
 			return (1);
-		}
 	}
 	return (0);
 }
